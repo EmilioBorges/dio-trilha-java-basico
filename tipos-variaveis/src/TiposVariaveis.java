4c2619f790722fb9fdc39e6f420259d87c7cc62b
emiliol <emilio.borges2012@gmail.com>
2022-10-20 11:38:18 -0300

    estudo sobre operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,18 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //tipos primitivos
+        //estudar a classe string que representa texto na aplicação
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal; //Casting
+
+        //VARIAVEIS X CONSTANTES(FINAL nao pode ser alterado e escrito em caixa alta)
+
+        int numero = 10;
+        final double VALOR_DE_PI = 3.14;
     }
 }
