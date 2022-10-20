a320b2457ff0df87f04ad65d69b6678748b53b4a
emiliol <emilio.borges2012@gmail.com>
2022-10-20 11:39:03 -0300

    operador ternario e relacional

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -19,5 +19,20 @@ public class Operadores {
 
     concatenacao = "1"+(1+1+1);
     System.out.println(concatenacao);
+
+    //operador ternario
+
+    int a = 5;
+    int b = 6;
+
+    String resultado = a>b ? "verdadeiro": "falso"; 
+    System.out.println(resultado);
+
+    //operadores relacionais
+
+    // == verificar a igualdade entre dois valores
+    // > verificar se o valor e da primeira variavel e maior que a segunda
+
+    
   }
 }
