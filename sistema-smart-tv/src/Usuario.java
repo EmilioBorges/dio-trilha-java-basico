36baf747759d9c934371b2866fdff3b82cbdf166
emiliol <emilio.borges2012@gmail.com>
2022-10-21 10:28:30 -0300

    Usando metodo para simular atividade de uma Smartv

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,29 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTV smartTV = new SmartTV();
+
+        smartTV.reduzirVolume();
+        smartTV.reduzirVolume();
+        smartTV.reduzirVolume();
+        smartTV.aumentarVolume();
+
+        System.out.println("Canal atual : " + smartTV.canal);
+
+        smartTV.mudarCanal(13);
+        System.out.println("Canal atual : " + smartTV.canal);
+
+        System.out.println("Volume atual : " + smartTV.volume);
+
+        System.out.println("TV ligada ? " + smartTV.ligada);
+        System.out.println("Canal atual : " + smartTV.canal);
+        System.out.println("Volume atual : " + smartTV.volume);
+
+        smartTV.ligar();
+
+        System.out.println("Nova estatus -> TV Ligada ? " + smartTV.ligada);
+
+        smartTV.desligar();
+        System.out.println("Nova estatus -> TV Ligada ? " + smartTV.ligada);
+
+    }
+}
